5264280b84a5ad890d5967b94542fa1282591e18
Salah <salahelsharkawy23@gmail.com>
2022-11-30 20:50:45 +0200

    read make and delete directories

--- coreModules.js
+++ coreModules.js
@@ -1,113 +1,44 @@
-//file system module
-//READ FILES
-const fs = require('fs')
-//console.log(fs);
-let data = fs.readFileSync('./message.txt')
-console.log(data);
-//<Buffer 68 65 6c 6c 6f 20 65 76 65 72 79 6f 6e 65>
-// الملف اتحفظ على هيئة صفر وواحد فلما اقراه لازم احدد التكويد االلي انا عايز احولهم ليه علشان اعرف اقراهم
-let DATA = fs.readFileSync('./message.txt','utf-8')
-console.log(DATA);
-console.log(data.toString());// default encoding of tostring is utf-8
-
-
-//sync = blocking
-//aync = non-blocking
-//async is better
-// any async fn must hv a callback fun
-let file = fs.readFile('./message.txt', 'utf-8',function (err, info) {
-    // دايما اول باراميتر في كل الدوال الغير متزامنة بتكون الخطا لو حصل 
-    // والتاني هنا هيبقى المعلومات اللي رجعت 
+const { log } = require('console');
+const fs = require('fs');
+//directories
+//make new dir
+fs.mkdir('newDir',(err) => {
     if (err) {
-        console.log('error occured',err);
+        console.log('err occured');        
     }else{
-        console.log(info);
+    process.chdir('./newDir')
+    fs.writeFile('./childFile.txt', 'thi is a file inside a directory',(err)=>{
+            if(err)
+            console.log('err occured');
+        })
+    }});
+
+
+
+
+//remove dir
+fs.rmdir('./newDir',{ recursive: true },(err)=>{
+    if (err) {
+        console.log('err');
+    }
+}); 
+
+
+
+
+
+
+
+
+
+
+//read dir
+fs.readdir('./',(err,files)=>{
+    if (err) {
+        console.log('error');
+    }else{
+        console.log(files);
     }
 })
-
-
-
-// WRITE FILES
-fs.writeFile('./new.txt','this a simple txt',function (err) {
-    if (err) {
-        console.log('error occured',err);
-}}); // new file 
-
-
-// APPEND FILE
-// لو عاوز ازود علىالملف كلام 
-//fs.appendFile
-//fs.appendFileSync
-fs.appendFile('./new.txt',' this a simple txt',function (err) {
-    if (err) {
-        console.log('error occured',err);
-}});
-
-//DELETE
-fs.unlink('./new.txt',function (err) {
-    if (err) {
-        console.log('error occured',err);
-}});
-
-
-// async 
-fs.writeFile('./a.txt','aaaaaaaaaaa',(err) => {
-    if (err) {
-        console.log('error occured',err);
-}});
-fs.appendFile('./a.txt','bbbbbbbbbbb',(err) => {
-    if (err) {
-        console.log('error occured',err);
-}});
-fs.writeFile('./a.txt','cccccccccccc',(err) => {
-    if (err) {
-        console.log('error occured',err);
-}});
-fs.writeFile('./a.txt','aaaaaaaaaaa',(err) => {
-    if (err) {
-        console.log('zzzzzzzzzzz',err);
-}});  
-//every time u run u get different file -> async
- 
-//sync to keep sequence
-// لو عندي ترتيب معين عايز امشي بيه 
-fs.writeFile('./b.txt','aaaaaaaaaaa',(err) => {
-    if (err) {
-    console.log('error occured',err);
-    fs.appendFile('./b.txt','bbbbbbbbbbb',(err) => {
-        if (err) {
-            console.log('error occured',err);
-            fs.appendFile('./b.txt','cccccccccccc',(err) => {
-                if (err) {
-                    console.log('error occured',err);
-            }});
-            
-    }});
-}});
-
-//note diiference 
-fs.writeFile('./c.txt','aaaaaaaaaaa',(err) => {
-    if (err) 
-    console.log('error occured',err);
-    else{fs.appendFile('./c.txt','bbbbbbbbbbb',(err) => {
-            if (err) 
-            console.log('error occured',err);
-                else{fs.appendFile('./c.txt','cccccccccccc',(err) => {
-                    if (err) {
-                    console.log('error occured',err);
-                    }
-                });}
-            
-            
-    });}
-});
-
-//note diiference 
-fs.writeFile('./d.txt','aaaaaaaaaaa',() => {
-    fs.appendFile('./d.txt','bbbbbbbbbbb',() => {
-                fs.appendFile('./d.txt','cccccccccccc',() => {
-                });   
-    });
-});
-// بعد ماتخلص انشا الملف وكتابة حرف الايه ضيف حرف البي ولما تخلص ضيف حرف السي 
-// طبعا ده بيحصل لان الدوال sync
+// fs.readdir take dir and return err and files that i take them
+// in a function a duse them
