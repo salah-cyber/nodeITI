8071e8f74827c2384eae09d4910348040474f59a
Salah <salahelsharkawy23@gmail.com>
2022-12-02 11:41:38 +0200

    handling pots form request

--- expressDemo/app.js
+++ expressDemo/app.js
@@ -3,6 +3,7 @@ const express = require('express') // express is a web app framework
 const app = express(); // express module is a fun that return app object 
 const path = require('path');
 
+
 const port = process.env.PORT||3000;
 const students = [
     {name:'sasa', course:'hesab',id:2},
@@ -12,27 +13,14 @@ const students = [
 
 
 
-
-
-//localhost:3000/
-app.listen(port,()=>{console.log(`listening....!!! port ${port}`)}); 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/main.html')); //contain form that ask to return welcome.html
-})
-//localhost:3000/welcome.html
-app.get('/welcome.html',(req,res)=>{
-    console.log(req.query);
-    res.sendFile(path.join(__dirname,'/welcome.html'));
-})
-
-
-
+//passing data from client side to server side 
+//1-url param   2-http requet body   3-query string ->?nm=x&age=10
+//1
 //sending json from api endpoint
 //handling all students request
 app.get('/api/students',(req,res)=>{
     res.json(students)
 })
-
 //handling A student request by id
 // passing data from client to server via url parameter
 app.get('/api/students/:id',(req,res)=>{
@@ -45,8 +33,22 @@ if (std) {
 }else{
     res.send('nOt FoUnD')
 }
-
 })
 
-//passing data from client side to server side 
-//1-url param   2-http requet body   3-query string ->?nm=x&age=10
+
+//3-
+//localhost:3000/
+app.listen(port,()=>{console.log(`listening....!!! port ${port}`)}); 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/main.html')); //contain form that ask to return welcome.html
+})
+//localhost:3000/welcome.html
+app.get('/welcome.html',(req,res)=>{
+    console.log(req.query);
+    res.sendFile(path.join(__dirname,'/welcome.html'));
+})
+app.use(express.urlencoded({extended:true}))
+app.post('/welcome.html',(req,res)=>{
+    //console.log(req.query);
+    res.send(`thanks ${req.body.fnm} ${req.body.lnm} for send requirind data`);
+})
