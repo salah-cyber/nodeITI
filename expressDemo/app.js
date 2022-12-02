6c7f002889ed1be407614c8c0f79f12b63711fa1
Salah <salahelsharkawy23@gmail.com>
2022-12-02 22:08:37 +0200

    validation by ajv

--- expressDemo/app.js
+++ expressDemo/app.js
@@ -1,93 +1,72 @@
-//entry file
-const express = require('express') // express is a web app framework
-const app = express(); // express module is a fun that return app object 
+const express = require('express') 
+const app = express();
 const path = require('path');
-const { toUnicode } = require('punycode');
-
+const Ajv = require("ajv")
+const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+//------------------------------------------------------------------------------
+const schema = {
+    type: "object",
+    properties: {
+      name: {type: "string",'pattern':'^[A-Za-z]*$'},
+      course: {type: "string",'maxLength':12,'minLength':2,'enum':['english','physiology','cs','it']}
+    },
+    required: ["name",'course'],
+    additionalProperties: false
+  }
+  let validate = ajv.compile(schema);
+//------------------------------------------------------------------------------
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
-
+//------------------------------------------------------------------------------
 const port = process.env.PORT||3000;
 app.listen(port,()=>{console.log(`listening....!!! port ${port}`)}); 
-
-
+//------------------------------------------------------------------------------
 const students = [
     {name:'sasa', course:'hesab',id:2},
     {name:'mona', course:'english',id:4},
     {name:'reda', course:'francais',id:1}
 ]
-
-
-
-//passing data from client side to server side 
-//1-url param   2-http requet body   3-query string ->?nm=x&age=10
-//1
-//sending json from api endpoint
-//handling all students request
+//------------------------------------------------------------------------------
 app.get('/api/students',(req,res)=>{
     res.set('access-control-Allow-Origin', '*'); 
     res.json(students)
 })
-
-
-//handling A student request by id
-// passing data from client to server via url parameter
+//------------------------------------------------------------------------------
 app.get('/api/students/:id',(req,res)=>{
-    let id = req.params.id; //i take id from url
-    let std =   students.find((val,index,arr)=>{ //find array method 
+    let id = req.params.id;
+    let std =   students.find((val,index,arr)=>{ 
                  return val.id ==id
                 })
-if (std) {
-    res.json(std)
-}else{
-    res.send('nOt FoUnD')
-}
+    if (std) {
+        res.json(std)
+    }else{
+        res.send('nOt FoUnD')
+    }
 })
-
-
-//localhost:3000/
+//------------------------------------------------------------------------------
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/main.html')); //contain form that ask to return welcome.html
+    res.sendFile(path.join(__dirname,'/main.html')); 
 })
-
-
-//localhost:3000/welcome.html
+//------------------------------------------------------------------------------
 app.get('/welcome.html',(req,res)=>{
-    //console.log(req.query);
     res.sendFile(path.join(__dirname,'/welcome.html'));
 })
-
-
-//request body
+//------------------------------------------------------------------------------
 app.post('/welcome.html',(req,res)=>{
-    //console.log(req.query);
     res.send(`thanks ${req.body.fnm} ${req.body.lnm} for send requirind data`);
 })
-
-
-
-//test rest api services
-//cmd as powershell
-//online as postwoman
-//3rd party tool as postman, fiddler 
-
-
-//poweshell
-//Invoke-webrequest -uri http://localhost:3000/ -method GET
-//(Invoke-webrequest -uri http://localhost:3000/ -method GET).Content
-//Invoke-RestMethod -uri http://localhost:3000/api/students -method GET
-//Invoke-RestMethod -uri http://localhost:3000/welcome.html -method POST -body @{fnm='lala';lnm='ahmed'}
-
-
-
-//make new student
+//------------------------------------------------------------------------------
 app.post('/api/students',(req,res)=>{
-    req.body.id = students.length+1;
-    students.push(req.body);
-    res.json(students);
+    const valid = validate(req.body);
+    if (valid) {
+        req.body.id = students.length+1;
+        students.push(req.body);
+        res.json(students);
+    } else {
+        res.status(403).send('forbidden data')
+    }
 })
-
-//delete student
+//------------------------------------------------------------------------------
 app.delete('/api/students/:id',(req,res)=>{
     let index = students.findIndex((val)=>{return val.id==req.params.id});
     if(index != -1){students.splice(index,1);}else{
@@ -96,8 +75,7 @@ app.delete('/api/students/:id',(req,res)=>{
     res.json(students);
     
 })
-
-//update student
+//------------------------------------------------------------------------------
 app.put('/api/students/:id',(req,res)=>{
     let index = students.findIndex((val)=>{return val.id==req.params.id});
     if(index != -1){
@@ -109,3 +87,4 @@ app.put('/api/students/:id',(req,res)=>{
         res.send('student not found .. update not allowed')
     }
 })
+//------------------------------------------------------------------------------
